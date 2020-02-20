55538507bad4b295601ca95ab8835c987b3f3d76
Alvaro Leal <pursuance@gmail.com>
2020-02-20 18:12:09 +0100

    Netlify auth

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as="h1">Song book</Heading>
-      <Button sx={{ marginTop: 2 }}>
-        Log in
-      </Button>
-    </Flex>
-  </Container>
-);
+
+export default props => {
+
+  useEffect(() => { netlifyIdentity.init({}); }, []);
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as="h1">Song book</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => netlifyIdentity.open()}
+        >
+          Sign up / Log in
+        </Button>
+      </Flex>
+    </Container>
+  );
+}
