6f5bf9df6da7212ea4b1d44bff04148fc24eaa82
Alvaro Leal <pursuance@gmail.com>
2020-03-02 01:01:22 +0100

    Update graphql server

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -2,13 +2,36 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    songs: [Song]
+  }
+  type Song {
+    id: ID!
+    name: String!
+    youtubeId: String
+  }
+  type Mutation {
+    addSong(): Song
+    updateSongDone(id: ID!): Song
   }
 `;
 
+let songIndex = 0;
+const songs = {};
 const resolvers = {
   Query: {
-    hello: () => 'Hello world'
+    songs: () => Object.values(songs)
+  },
+  Mutation: {
+    addSong: (_, {name, youtubeId}) => {
+      songIndex++;
+      const id = `key-${songIndex}`;
+      songs[id] = { id, name, youtubeId };
+      return songs[id];
+    }
+  },
+  updateSongDone: (_, {id, name, youtubeId}) => {
+    songs[id] = { id, name, youtubeId };
+    return songs[id];
   }
 };
 
