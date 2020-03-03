eb966725b95263e5fd72f5308f857603fe38ff49
Alvaro Leal <pursuance@gmail.com>
2020-03-03 01:32:47 +0100

    Add fetch polyfill

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,20 +1,2 @@
-const React = require('react');
 const wrapRootElement = require('./wrap-root');
-const {
-  ApolloProvider,
-  ApolloClient,
-  HttpLink,
-  InMemoryCache
-} = require('@apollo/client');
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'https://song-book.netlify.com/.netlify/functions/index'
-  })
-});
-exports.wrapRootElement = ({ element}) => (
-  <ApolloProvider client={client}>
-    {wrapRootElement({ element})}
-  </ApolloProvider>
-);
+exports.wrapRootElement = wrapRootElement;
--- packages/www/wrap-root.js
+++ packages/www/wrap-root.js
@@ -6,9 +6,26 @@ const updatedTheme = {
   ...future,
   sizes: { container: 1024 }
 };
+const fetch = require('node-fetch');
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache
+} = require('@apollo/client');
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://song-book.netlify.com/.netlify/functions/index',
+    fetch
+  })
+});
 
 module.exports = ({ element }) => (
   <IdentityProvider>
-    <ThemeProvider theme={updatedTheme}>{element}</ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={updatedTheme}>{element}</ThemeProvider>
+    </ApolloProvider>
   </IdentityProvider>
 );
