47b47f73b3ac76dc966b350e37229b3928429879
Alvaro Leal <pursuance@gmail.com>
2020-03-09 02:19:49 +0100

    Remove comments

--- packages/www/wrap-root.js
+++ packages/www/wrap-root.js
@@ -1,7 +1,6 @@
 const React = require('react');
 const { ThemeProvider } = require('theme-ui');
 const { future } = require('@theme-ui/presets');
-// const { IdentityProvider } = require('./identity-context');
 const updatedTheme = {
   ...future,
   sizes: { container: 1024 }
@@ -13,26 +12,11 @@ const {
   HttpLink,
   InMemoryCache
 } = require('@apollo/client');
-// const { setContext } = require("apollo-link-context");
-// const netlifyIdentity = require("netlify-identity-widget");
 
 const httpLink = new HttpLink({
   uri: 'https://song-book.netlify.com/.netlify/functions/index',
   fetch
 });
-
-// const authLink = setContext((_, { headers }) => {
-//   const user = netlifyIdentity.currentUser();
-//   const token = user && user.token.access_token;
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       user: user && user.id,
-//       Authorization: token ? `Bearer ${token}` : ''
-//     }
-//   };
-// });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -40,9 +24,7 @@ const client = new ApolloClient({
 });
 
 module.exports = ({ element }) => (
-  // <IdentityProvider>
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={updatedTheme}>{element}</ThemeProvider>
-    </ApolloProvider>
-  // </IdentityProvider>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={updatedTheme}>{element}</ThemeProvider>
+  </ApolloProvider>
 );
